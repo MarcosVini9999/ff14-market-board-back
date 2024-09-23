0a2a42958d51386ca6249957a6d79a84f4340e57
Marcos Vinicius Andrade <marcosviniciusandradedesousa@hotmail.com>
2024-09-23 14:42:40 -0300

    feat: swagger and cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('FF14 Marketboard API')
+    .setDescription(
+      'API for querying items from the Market Board of the Final Fantasy XIV game',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
